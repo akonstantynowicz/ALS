8c9a7fe38c40aafdf04abb540b9f7d55fe579ae4
akonstantynowicz <konsiax@gmail.com>
2019-12-06 19:17:21 +0100

    transponowanie macierzy

--- src/pl/ug/edu/Main.java
+++ src/pl/ug/edu/Main.java
@@ -2,6 +2,8 @@ package pl.ug.edu;
 
 import pl.ug.edu.data.Parser;
 import pl.ug.edu.data.Review;
+import pl.ug.edu.gauss.Gauss;
+import pl.ug.edu.gauss.Macierz;
 
 import java.io.IOException;
 import java.util.List;
@@ -9,11 +11,19 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args){
-//        Gauss gauss = new Gauss();
-//        Macierz m = new Macierz(5);
-//        m.drukuj();
-//        System.out.println(gauss.PG(m.macierz, m.wektor));
-//        m.drukuj();
+       /* Gauss gauss = new Gauss();
+        Macierz m = new Macierz(5,5);
+        m.drukuj();
+        m.transponujMacierz();
+        m.obliczWektor();
+        System.out.println();
+        m.drukuj();
+        System.out.println(gauss.PG(m.macierz, m.wektor));
+        for (int i=0;i<5;i++){
+            System.out.print(m.wektorX[i] + " ");
+        }
+        System.out.println("\n");
+        m.drukuj();*/
 
         Parser p = new Parser();
 
