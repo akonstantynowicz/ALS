79bc4e662c4a3d947a043fb6d1edbf09d86bf1a1
Jakub Ściślewski <32902950+jscislewski@users.noreply.github.com>
2019-12-08 14:13:34 +0100

    add matrix

--- src/pl/ug/edu/Main.java
+++ src/pl/ug/edu/Main.java
@@ -1,10 +1,12 @@
 package pl.ug.edu;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import pl.ug.edu.data.Matrix;
 import pl.ug.edu.data.Parser;
 import pl.ug.edu.data.Review;
-
-import java.io.IOException;
-import java.util.List;
 
 public class Main {
 
@@ -15,6 +17,7 @@ public class Main {
 //        System.out.println(gauss.PG(m.macierz, m.wektor));
 //        m.drukuj();
 
+      Matrix matrix = new Matrix();
         Parser p = new Parser();
 
         try {
@@ -22,11 +25,30 @@ public class Main {
 
             for (Review r : list) {
                 System.out.println(r);
+              if (!matrix.userList.containsKey(r.getUserId())) {
+                System.out.println("Dodawanie nowego usera do listy");
+                matrix.userList.put(r.getUserId(), matrix.userRatingsList.size());
+                matrix.userRatingsList.add(new ArrayList<>(Collections.nCopies(548552, 0)));
+              } else {
+                System.out.println("User już istnieje");
+              }
+              if (matrix.userList.containsKey(r.getUserId())) {
+                matrix.userRatingsList.get(matrix.userList.get(r.getUserId()))
+                    .set(r.getProductId(), r.getRating());
+              }
             }
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+      System.out.println("user 0");
+      for (int i = 0; i < 5; i++) {
+        System.out.println(matrix.userRatingsList.get(0).get(i));
+      }
+      System.out.println("user 1");
+      for (int i = 0; i < 5; i++) {
+        System.out.println(matrix.userRatingsList.get(1).get(i));
+      }
+
 
     }
 }
