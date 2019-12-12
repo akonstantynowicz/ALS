0add01d90a1330988b84289467889e04dd924a86
szczep4niak <jjdroid6969@gmail.com>
2019-12-12 14:42:23 +0100

    fixed

--- src/pl/ug/edu/als/ALS.java
+++ src/pl/ug/edu/als/ALS.java
@@ -5,11 +5,7 @@
 package pl.ug.edu.als;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 import pl.ug.edu.data.DataUtil;
 import pl.ug.edu.data.Parser;
@@ -17,8 +13,6 @@ import pl.ug.edu.data.Review;
 import pl.ug.edu.gauss.Gauss;
 import pl.ug.edu.gauss.Matrix;
 import pl.ug.edu.generic.Double;
-
-import javax.xml.crypto.Data;
 
 public class ALS {
 
@@ -79,12 +73,6 @@ public class ALS {
         }
     }
 
-    private void generateUserRatingsFromReviewList(List<Review> reviewList) {
-        for (Review review : reviewList) {
-            System.out.println(review);
-            addUserRating(review);
-        }
-    }
 
     private void setProductsAmount(int productsAmount) {
         this.productsAmount = productsAmount;
@@ -94,20 +82,6 @@ public class ALS {
         addUserIfNotInList(review.getUserId());
         userRatingsList.get(userList.get(review.getUserId()))
                 .set(review.getProduct().getProductId(), review.getRating());
-    }
-
-    private void addUserIfNotInList(final String userId) {
-        if (userList.containsKey(userId)) {
-            System.out.println("User już istnieje");
-        } else {
-            addUserToList(userId);
-        }
-    }
-
-    private void addUserToList(final String userId) {
-        System.out.println("Dodawanie nowego usera do listy");
-        userList.put(userId, userRatingsList.size());
-        userRatingsList.add(new ArrayList<>(Collections.nCopies(productsAmount, 0)));
     }
 
     private void alg() {
@@ -186,30 +160,33 @@ public class ALS {
         p = new Matrix(d, productsAmount);
         p.generateRandomMatrix();
         p.print();
+    }
 
-  /**
-   * This method runs all other methods in algorithm one by one.
-   * @throws IOException On input error.
-   * @see IOException
-   */
-  public void runAlsAlgorithm() throws IOException {
-    List<Review> reviewList = Parser.parseFile("sample.txt");
-    //System.out.println(DataUtil.getHighestProductId(reviewList));
-    setProductsAmount(DataUtil.getHighestProductId(reviewList) + 1);
-    long startTime = System.currentTimeMillis();
-    generateUserRatingsFromReviewList(reviewList);
-    generateRandomPMatrix();
-    generateRandomUMatrix();
-    alg();
-    System.out.println(
-        "\nd = " + d + "\nTime: " + ((System.currentTimeMillis() - startTime)
-            / MILISECONDS_IN_SECOND) + "s\n\n");
-  }
+    /**
+     * This method runs all other methods in algorithm one by one.
+     *
+     * @throws IOException On input error.
+     * @see IOException
+     */
+    public void runAlsAlgorithm() throws IOException {
+        List<Review> reviewList = Parser.parseFile("sample.txt");
+        //System.out.println(DataUtil.getHighestProductId(reviewList));
+        setProductsAmount(DataUtil.getHighestProductId(reviewList) + 1);
+        long startTime = System.currentTimeMillis();
+        generateUserRatingsFromReviewList(reviewList);
+        generateRandomPMatrix();
+        generateRandomUMatrix();
+        alg();
+        System.out.println(
+                "\nd = " + d + "\nTime: " + ((System.currentTimeMillis() - startTime)
+                        / MILISECONDS_IN_SECOND) + "s\n\n");
+    }
 
-  private void generateUserRatingsFromReviewList(List<Review> reviewList) {
-    for (Review review : reviewList) {
-      //System.out.println(review);
-      addUserRating(review);
+    private void generateUserRatingsFromReviewList(List<Review> reviewList) {
+        for (Review review : reviewList) {
+            //System.out.println(review);
+            addUserRating(review);
+        }
     }
 
     private void generateRandomUMatrix() {
@@ -236,109 +213,20 @@ public class ALS {
                         "\n" + Arrays.toString(userResultsAsNames));
     }
 
-  private void addUserRating(final Review review) {
-    addUserIfNotInList(review.getUserId());
-    userRatingsList.get(userList.get(review.getUserId()))
-        .set(review.getProduct().getProductId(), review.getRating());
-  }
 
-  private void addUserIfNotInList(final String userId) {
-    if (userList.containsKey(userId)) {
-      //System.out.println("User już istnieje");
-    } else {
-      addUserToList(userId);
+    private void addUserIfNotInList(final String userId) {
+        if (userList.containsKey(userId)) {
+            //System.out.println("User już istnieje");
+        } else {
+            addUserToList(userId);
+        }
     }
-  }
 
-  private void addUserToList(final String userId) {
-    //System.out.println("Dodawanie nowego usera do listy");
-    userList.put(userId, userRatingsList.size());
-    userRatingsList.add(new ArrayList<>(Collections.nCopies(productsAmount, 0)));
-  }
-
-  private void alg() {
-    calculatePAndUMatrixes();
-    resultMatrix = generateResultMatrix();
-    System.out.println("Result Matrix:");
-    resultMatrix.print();
-  }
-
-  private void calculatePAndUMatrixes() {
-    for (int k = 0; k < NUMBER_OF_ITERATIONS; k++) {
-      calculateP();
-      //System.out.println("U");
-      //u.print();
-      //System.out.println("P");
-      calculateU();
-      // p.print();
+    private void addUserToList(final String userId) {
+        //System.out.println("Dodawanie nowego usera do listy");
+        userList.put(userId, userRatingsList.size());
+        userRatingsList.add(new ArrayList<>(Collections.nCopies(productsAmount, 0)));
     }
-  }
 
-  private Matrix generateResultMatrix() {
-    Matrix uTransposed = u.transpose();
-    return uTransposed.multiply(p);
-  }
 
-  private void calculateP() {
-    for (int i = 0; i < productsAmount; i++) {
-      final List<Integer> ratingUsersIds = DataUtil.getRatingUserIds(userRatingsList, i);
-      final List<Integer> productRatings = DataUtil
-          .getProductRatings(userRatingsList, i);
-
-      final Matrix BU = calculateXU(ratingUsersIds, u);
-      BU.calculateVector(productRatings, ratingUsersIds, u, i);
-
-      calculateColumnValues(i, BU, p);
-    }
-  }
-
-  private void calculateU() {
-    int userIndex = 0;
-    for (final List<Integer> userRatings : userRatingsList) {
-      final ArrayList<Integer> ratedProductIds = (ArrayList<Integer>) DataUtil
-          .getRatedProductsIds(userRatings);
-
-      Matrix AU = calculateXU(ratedProductIds, p);
-      AU.calculateVector(userRatings, ratedProductIds, p);
-
-      calculateColumnValues(userIndex, AU, u);
-      userIndex++;
-    }
-  }
-
-  private Matrix calculateXU(List<Integer> ratedProductIds, final Matrix x) {
-    final Matrix XIx = new Matrix(d, ratedProductIds.size());
-    fillWithValues(ratedProductIds, XIx, x);
-    final Matrix XIxT = XIx.transpose();
-
-    final Matrix E = new Matrix(d, d);
-    E.generateUnitMatrix();
-    E.multiply(Double.valueOf(lambda));
-
-    return XIx.multiply(XIxT).add(E);
-  }
-
-  private void fillWithValues(List<Integer> idsList, Matrix X, Matrix x) {
-    for (int m = 0; m < d; m++) {
-      int i = 0;
-      for (int id : idsList) {
-        X.matrix[m][i] = x.matrix[m][id];
-        i++;
-      }
-    }
-  }
-
-  private void generateRandomPMatrix() {
-    // System.out.println("Generuje P");
-    p = new Matrix(d, productsAmount);
-    p.generateRandomMatrix();
-    // p.print();
-  }
-
-  private void generateRandomUMatrix() {
-    // System.out.println("Generuje U");
-    u = new Matrix(d, userList.size());
-    u.generateRandomMatrix();
-    //u.print();
-  }
 }
